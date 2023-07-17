4209af88c7f65ce264673caf7fce2506fa453827
Enes Usta <enesusta@protonmail.com>
2023-07-17 19:19:53 +0300

    KI-3 feat: add jackson and easy-random to populate sample data which will work on

--- src/main/java/com/github/enesusta/Kirpi.java
+++ src/main/java/com/github/enesusta/Kirpi.java
@@ -1,30 +1,42 @@
 package com.github.enesusta;
 
 
-import java.io.BufferedInputStream;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.enesusta.model.Employee;
+import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
+
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.OutputStream;
 
 public class Kirpi {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
 
-        try (InputStream inputStream = new BufferedInputStream(Files.newInputStream(Paths.get("data.txt")))) {
+        EasyRandomParameters parameters = new EasyRandomParameters();
+        EasyRandom generator = new EasyRandom(parameters);
+        ObjectMapper objectMapper = new ObjectMapper();
 
-            byte[] bytes = inputStream.readNBytes(61);
-            System.out.println("bytes = " + new String(bytes));
-
-            inputStream.skip(1);
-
-            byte[] bytes1 = inputStream.readNBytes(91);
-            System.out.println("bytes1 = " + new String(bytes1));
-
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        try (OutputStream outputStream = new FileOutputStream("data.txt")) {
+            for (int i = 0; i < 15; i++) {
+                Employee employee = generator.nextObject(Employee.class);
+                String s = objectMapper.writeValueAsString(employee);
+                outputStream.write((s + "\n").getBytes());
+            }
         }
 
-        Thread.currentThread().join();
+//        try (InputStream inputStream = new BufferedInputStream(Files.newInputStream(Paths.get("data.txt")))) {
+//            byte[] bytes = inputStream.readNBytes(61);
+//            System.out.println("bytes = " + new String(bytes));
+//
+//            inputStream.skip(1);
+//
+//            byte[] bytes1 = inputStream.readNBytes(91);
+//            System.out.println("bytes1 = " + new String(bytes1));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+
+//        Thread.currentThread().join();
     }
 }
---
+++ src/main/java/com/github/enesusta/model/Employee.java
@@ -0,0 +1,40 @@
+package com.github.enesusta.model;
+
+public class Employee {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private Department department;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
